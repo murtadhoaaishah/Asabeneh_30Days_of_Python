bb5fd7126d1877f7bf20f3dcd527d1e98632785b
Aaishah <aishatmurtadho@gmail.com>
2024-07-02 12:33:33 +0100

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,56 @@
+# Exercises
+#level 1: Check the python version you are using
+# 1. python --version
+#Number 2: Write strings on the python interactive shell
+3 + 4
+3 - 4
+3 * 4
+3 % 4
+3 / 4
+3 ** 4
+3 // 4
+
+#Number 3: Write strings on the python interactive shell
+'Aaiahah'
+'Murtadho'
+'Nigeria'
+'I am enjoying 30 days of python'
+
+#Number 4: Check the data types of the following data:
+type(10)
+type(9.8)
+type(3.4)
+type(4 - 4j)
+type(10)
+type(['Asabeneh', 'Python', 'Finland'])
+type('Aaiahah')
+type('Murtadho')
+type('Nigeria')
+
+#level 2:
+#Number 1
+'''Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it.'''
+
+#Level 3
+#Number 1: Write an example for different Python data types 
+
+4 #Number: integeer
+-3.5, 6.5 #Number: float
+1 + j, 2 + 4j  #Number:complex
+'Asabeneh' #string
+True, False #boolean
+[0, 1, 2, 3, 4, 5] #list
+{
+'first_name':'Asabeneh',
+'age':250, 
+'is_married':True,
+'skills':['JS', 'React', 'Node', 'Python']
+}  #Dictionary
+('Asabeneh', 'Pawel', 'Brook', 'Abraham', 'Lidiya') #tuple
+{2, 4, 3, 5} #set
+
+
+import math
+point1 = (1, 2)
+point2 = (4, 6)
+euclidean_distance = math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
---
+++ day1/helloworld.py
@@ -0,0 +1,24 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+#Number 3: Write strings on the python interactive shell
+print('Aaiahah')
+print('Murtadho')
+print('Nigeria')
+print('I am enjoying 30 days of python')
+
+#Number 4: Check the data types of the following data:
+type(10)
+type(9.8)
+type(3.4)
+type(4 - 4j)
+type(10)
+type(['Asabeneh', 'Python', 'Finland'])
+type('Aaiahah')
+type('Murtadho')
+type('Nigeria')
